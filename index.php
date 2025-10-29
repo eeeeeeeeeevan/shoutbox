ed97932a87814e8b35859d86f757807fef637360
evan <im@evan.lat>
2025-10-29 13:07:36 +1100

    rem

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>shout</title>
-    <script src="./assets/tw.js"></script>
     <link rel="stylesheet" href="./assets/main.css">
 </head>
 
